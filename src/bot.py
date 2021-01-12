30d0ad79b0476e2b39143167552f6256c6752e83
sryvcr <andresgx9@gmail.com>
2021-01-11 19:46:13 -0500

    youtube command to search videos in youtube added

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,7 @@
 import os
+import re
 import datetime
+from urllib import parse, request
 
 import discord
 from discord.ext import commands
@@ -25,12 +27,26 @@ async def sum(ctx, numOne: int, numTwo: int):
 async def stats(ctx):
     embed = discord.Embed(title=f"{ctx.guild.name}", description="Server stats",
                           timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
-    embed.add_field(name="Server created at", value=f"{ctx.guild.created_at}")
-    embed.add_field(name="Server owner", value=f"{ctx.guild.owner}")
-    embed.add_field(name="Server region", value=f"{ctx.guild.region}")
-    embed.add_field(name="Server ID", value=f"{ctx.guild.id}")
+    embed.add_field(name="Server ID:", value=f"{ctx.guild.id}")
+    embed.add_field(name="Server owner:", value=f"{ctx.guild.owner}")
+    embed.add_field(name="Server region:", value=f"{ctx.guild.region}")
+    embed.add_field(name="Server created at:", value=f"{ctx.guild.created_at}")
     embed.set_thumbnail(url=f"{ctx.guild.icon_url}")
     await ctx.send(embed=embed)
+
+
+@bot.command()
+async def youtube(ctx, *, search: str):
+    """
+        this function does a search in youtube and return to discord the first 10 result
+    """
+    query_string = parse.urlencode({'search_query': search})
+    html_content = request.urlopen(
+        f'http://www.youtube.com/results?{query_string}')
+    search_results = re.findall(
+        r'/watch\?v=(.{11})', html_content.read().decode())
+    for i in range(10):
+        await ctx.send('https://www.youtube.com/watch?v=' + search_results[i])
 
 
 # Events
