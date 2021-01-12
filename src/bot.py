d85194ba8d3350875e129afe4c27a4b6472800f2
sryvcr <andresgx9@gmail.com>
2021-01-11 19:12:24 -0500

    sum and stats commands added

--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 import discord
 from discord.ext import commands
@@ -13,6 +14,23 @@ bot = commands.Bot(command_prefix='>', description="This is a PyBot")
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+
+@bot.command()
+async def sum(ctx, numOne: int, numTwo: int):
+    await ctx.send(f"{numOne} + {numTwo} = {numOne + numTwo}")
+
+
+@bot.command()
+async def stats(ctx):
+    embed = discord.Embed(title=f"{ctx.guild.name}", description="Server stats",
+                          timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
+    embed.add_field(name="Server created at", value=f"{ctx.guild.created_at}")
+    embed.add_field(name="Server owner", value=f"{ctx.guild.owner}")
+    embed.add_field(name="Server region", value=f"{ctx.guild.region}")
+    embed.add_field(name="Server ID", value=f"{ctx.guild.id}")
+    embed.set_thumbnail(url=f"{ctx.guild.icon_url}")
+    await ctx.send(embed=embed)
 
 
 # Events
