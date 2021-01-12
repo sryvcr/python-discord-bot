97fbd2f70b727a13e30f5dc982cc16bd1150ca4c
sryvcr <andresgx9@gmail.com>
2021-01-11 20:27:11 -0500

    pokedex command to get pokemon info is added

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,7 @@
 import os
 import re
 import datetime
+import requests
 from urllib import parse, request
 
 import discord
@@ -47,6 +48,34 @@ async def youtube(ctx, *, search: str):
         r'/watch\?v=(.{11})', html_content.read().decode())
     for i in range(10):
         await ctx.send('https://www.youtube.com/watch?v=' + search_results[i])
+
+
+@bot.command()
+async def pokedex(ctx, *, search_by: str or int):
+    """
+        this function return to discord a pokedex result
+    """
+    pokemon_res = requests.get(f'https://pokeapi.co/api/v2/pokemon/{search_by}').json()
+    pokemon_species_res = requests.get(f'https://pokeapi.co/api/v2/pokemon-species/{search_by}').json()
+    abilities = list(map(lambda x: x['ability']['name'], pokemon_res['abilities']))
+    description = pokemon_species_res['flavor_text_entries'][0]['flavor_text']
+    types = list(map(lambda x: x['type']['name'], pokemon_res['types']))
+    stats = list(map(lambda x: x['stat']['name'] + ": " + str(x['base_stat']), pokemon_res['stats']))
+    embed = discord.Embed(title=f"Pokedex", description="Pokemon info", color=discord.Color.dark_green())
+    embed.add_field(name="Pokedex ID:", value=f"{pokemon_res['id']}")
+    embed.add_field(name="Name:", value=f"{pokemon_res['name']}")
+    embed.add_field(name="Types:", value=f"{', '.join(types)}")
+    embed.add_field(name="Description:", value=f"{description}")
+    embed.add_field(name="Abilities:", value=f"{', '.join(abilities)}")
+    embed.add_field(name="Stats:", value=f"{', '.join(stats)}")
+    embed.add_field(name="Color:", value=f"{pokemon_species_res['color']['name']}")
+    embed.add_field(name="Height:", value=f"{pokemon_res['height']}")
+    embed.add_field(name="Weight:", value=f"{pokemon_res['weight']}")
+    embed.add_field(name="Base experience:", value=f"{pokemon_res['base_experience']}")
+    embed.add_field(name="Capture rate:", value=f"{pokemon_species_res['capture_rate']}")
+    embed.add_field(name="Habitat:", value=f"{pokemon_species_res['habitat']['name']}")
+    embed.set_thumbnail(url=f"{pokemon_res['sprites']['front_default']}")
+    await ctx.send(embed=embed)
 
 
 # Events
